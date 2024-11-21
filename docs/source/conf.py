2dc67981f66c636854597a9321f2e430811049e3
Joe Schulze <43186618+schulze61@users.noreply.github.com>
2024-11-21 12:40:47 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ExoPlex'
+copyright = '2024, GUnterborn'
+author = 'Cayman Unterborn'
 
 release = '0.1'
 version = '0.1.0'
